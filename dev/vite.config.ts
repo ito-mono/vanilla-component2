7d290089102a77f8abbdfe926f09283fa9f87dc1
Toshiki Ito <ito@monoworks.co.jp>
2024-01-23 08:12:21 +0900

    button作成

--- dev/vite.config.ts
+++ dev/vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), dts()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, 'src'),
+    },
+  },
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'index',
+      fileName: 'index',
+      formats: ['es'],
+    },
+    outDir: '../dist',
+    emptyOutDir: true,
+  },
+});
